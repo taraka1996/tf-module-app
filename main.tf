f3915aa2422747a5073104a17dcd911e49650363
TARAK <tarakaramtirumala@gmail.com>
2023-03-28 10:54:52 +0530

    changes

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_launch_template" "foo" {
+  name = "${var.component}-${var.env}"
+
+ 
+  image_id = "data.aws_ami.ami.id"
+  
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  instance_type = var.instance_type
+
+  tag_specifications {
+    resource_type = "instance"
+
+   tags = merge(
+        var.tags, 
+        { Name = "${var.component}- ${var.env}" }
+    )
+  }
+
+ }
---
+++ vars.tf
@@ -0,0 +1,4 @@
+variable "env" {}
+variable "tags" {}
+variable "component" {}
+variable "instance_type" {}
