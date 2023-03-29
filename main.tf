7cf1118b023ee9ebadfaaba323326778c9d30f99
TARAK <tarakaramtirumala@gmail.com>
2023-03-29 07:52:28 +0530

    changes

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_launch_template" "main" {
   }
 
   instance_type = var.instance_type
+  vpc_security_group_ids = ["aws_security_group.main.id"]
 
   tag_specifications {
     resource_type = "instance"
@@ -39,3 +40,37 @@ resource "aws_launch_template" "main" {
   }
 
  } 
+ resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}"
+  description = "${var.component}-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
+  ingress {
+    description = "APP"
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.allow_app_to
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
--- vars.tf
+++ vars.tf
@@ -6,3 +6,5 @@ variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
 variable "subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
