2658e6ed780288c0d7f1abd9b6e81ad071e71c38
TARAK <tarakaramtirumala@gmail.com>
2023-03-29 11:06:04 +0530

    changes

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_policy" "main" {
         //          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.elasticache.*"
         //          /// How to limit this permissions, which are unwanted for all components
         //        ]
-        Resource : [for k in local.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${k}.*"]
+        Resource : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*"
       },
       {
         "Sid" : "VisualEditor1",
