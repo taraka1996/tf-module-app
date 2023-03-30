c7b8e17c9ceaf18193f0672f3865ccd39dcd5c0d
TARAK <tarakaramtirumala@gmail.com>
2023-03-30 12:53:23 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-local {
+locals {
   dns_word = var.env == "prod" ? "www" : var.env
   dns_name = var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
 
