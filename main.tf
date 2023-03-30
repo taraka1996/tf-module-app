43d4911031bce8a4479fe376ee2e3d1161b351a4
TARAK <tarakaramtirumala@gmail.com>
2023-03-30 11:52:41 +0530

    changes

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_launch_template" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
   
 
   launch_template {
@@ -73,6 +74,24 @@ resource "aws_launch_template" "main" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+    interval            = 5
+    timeout             = 4
+    path                = "/health"
+  }
   tags = merge(
     var.tags,
     { Name = "${var.component}-${var.env}" }
