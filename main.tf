410c4908d93ffcf9795e7929691ef0e7afb10856
TARAK <tarakaramtirumala@gmail.com>
2023-04-04 22:51:49 +0530

    changes

--- main.tf
+++ main.tf
@@ -43,14 +43,25 @@ resource "aws_launch_template" "main" {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
-   tag {
+  tag {
     key                 = "Name"
     propagate_at_launch = true
     value               = "${var.component}-${var.env}"
   }
 }
+  
+  resource "aws_autoscaling_policy" "asg-cpu-rule" {
+   name                   = "CPULoadDetect"
+   autoscaling_group_name = aws_autoscaling_group.main.name
+   policy_type            = "TargetTrackingScaling"
+   target_tracking_configuration {
+     predefined_metric_specification {
+       predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
 
-
+   
  resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}"
   description = "${var.component}-${var.env}"
