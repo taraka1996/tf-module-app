2b379307ad38076098c47dc3f17917a982f47de2
TARAK <tarakaramtirumala@gmail.com>
2023-04-26 10:44:53 +0530

    changes

--- main.tf
+++ main.tf
@@ -118,6 +118,7 @@ resource "aws_lb_target_group" "main" {
     timeout             = 4
     path                = "/health"
   }
+  deregistration_delay = "30"
   tags = merge(
       var.tags,
       { Name = "${var.component}-${var.env}", Monitor = "yes" }
